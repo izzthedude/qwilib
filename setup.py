1d154e80ed85943a93bba12e5ea2c880b3b1f242
Izzat Z <izzat.zainir11@gmail.com>
2022-08-26 18:36:00 +0800

    Update setup.py to include resources and use Alpha versioning

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from setuptools import setup
 
 PROJECT_NAME = "qwilib"
 PROJECT_ROOT = Path(__file__).parent
-SOURCE_ROOT = PROJECT_ROOT / "qwilib"
+SOURCE_ROOT = PROJECT_ROOT / PROJECT_NAME
+RESOURCES = f"{str(SOURCE_ROOT / 'resources' / 'icons')}/*"
 
 with open(str(PROJECT_ROOT / "requirements.txt"), "r") as file:
     REQUIREMENTS = file.readlines()
 
 setup(
     name=PROJECT_NAME,
-    version="0.1.0",
+    version="0.1.0a1",
     author="Izzat Z.",
     author_email="<izzat.zainir11@gmail.com>",
     description="An opinionated Qt Widgets library.",
@@ -19,6 +20,8 @@ setup(
     long_description="A Python library that attempts to pre-define some Qt Widgets (based on Pyside) for ease of "
                      "development.",
     packages=[PROJECT_NAME],
+    package_data={PROJECT_NAME: [RESOURCES]},
+    include_package_data=True,
     install_requires=REQUIREMENTS,
     keywords=[
         "python",
